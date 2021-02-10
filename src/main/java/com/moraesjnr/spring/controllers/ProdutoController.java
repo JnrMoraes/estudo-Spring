744ba9495d7e523b691e6bb8982a29e0f9340845
juniormoraes <contatomoraesjr@gmail.com>
2021-02-09 22:12:38 -0300

    arrumado anotação, delete e paginação

--- src/main/java/com/moraesjnr/spring/controllers/ProdutoController.java
+++ src/main/java/com/moraesjnr/spring/controllers/ProdutoController.java
@@ -3,12 +3,18 @@ package com.moraesjnr.spring.controllers;
 import com.moraesjnr.spring.model.entities.Produto;
 import com.moraesjnr.spring.model.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/produtos")
@@ -17,10 +23,39 @@ public class ProdutoController {
     @Autowired
     private ProdutoRepository produtoRepository;
 
-    @PostMapping
+//    @PostMapping
+    @RequestMapping(method = { RequestMethod.POST, RequestMethod.PUT })
     public @ResponseBody Produto novoProduto(@Valid Produto produto){
         produtoRepository.save(produto);
-
         return produto;
     }
+
+    @GetMapping
+    public Iterable<Produto> obterProdutos(){
+        return produtoRepository.findAll();
+    }
+
+    @GetMapping(path = "/pagina/{numeroPagina}")
+    public Iterable<Produto> obterProdutosPorPagina(
+            @PathVariable int numeroPagina){
+        Pageable page = PageRequest.of(0, 3);
+        return produtoRepository.findAll(page);
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<Produto> obterProduto(@PathVariable int id){
+       return produtoRepository.findById(id);
+    }
+
+//    @PutMapping
+//    public Produto alterarProduto(@Valid Produto produto){
+//        produtoRepository.save(produto);
+//        return produto;
+//    }
+
+    @DeleteMapping(path = "/{id}")
+    public void excluirProduto(@PathVariable int id){
+        produtoRepository.deleteById(id);
+    }
+
 }
--- src/main/java/com/moraesjnr/spring/model/repositories/ProdutoRepository.java
+++ src/main/java/com/moraesjnr/spring/model/repositories/ProdutoRepository.java
@@ -1,9 +1,10 @@
 package com.moraesjnr.spring.model.repositories;
 
 import com.moraesjnr.spring.model.entities.Produto;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
+public interface ProdutoRepository extends PagingAndSortingRepository<Produto, Integer> {
+
 
 
 }
